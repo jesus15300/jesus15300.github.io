5b802815527030992cd5f62f3d819f025c5561e6
Jesus153 <jesusemanuellive@gmail.com>
2025-01-14 13:48:15 -0600

    Cambios de loader en lista

--- invitacion-itza/js/obtener-invitados-confirmados.js
+++ invitacion-itza/js/obtener-invitados-confirmados.js
@@ -1,5 +1,6 @@
-async function obtenerInvitadosConfirmados(){
+async function obtenerInvitadosConfirmados(loader){
     try {
+      showLoader(loader);
         // Envía los datos al backend como JSON
         const response = await fetch(apiUrl + 'api/invitaciones/obtener-confirmados?id=1', {
           method: 'GET',
@@ -10,12 +11,21 @@ async function obtenerInvitadosConfirmados(){
   
         if (response.ok) {
           const result = await response.json();
+          hideLoader(loader)
           return result;
         } else {
           alert('Error al obtener los datos, contacte a Chucho :(');
+          hideLoader(loader);
         }
       } catch (error) {
         console.error('Error:', error);
         alert('Hubo un error al recibir los datos, no entre en panico y llame a Chucho.');
+        hideLoader(loader);
       }
+}
+function showLoader(loader){
+    loader.classList.remove('disabled');
+}
+function hideLoader(loader){
+    loader.classList.add('disabled');
 }
