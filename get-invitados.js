1463e654c75ca991274044561ccc5e850eab0f3e
Jesus153 <jesusemanuellive@gmail.com>
2025-01-18 21:38:46 -0600

    algunas funciones para el ordenado automatico

---
+++ get-invitados.js
@@ -0,0 +1,164 @@
+function obtenerConfirmaciones(){
+    return {
+        "confirmados": [
+            {
+                "id": 1,
+                "nombre": "Jesús Emanuel Salgado",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-13T23:10:42.059618",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 5,
+                "nombre": "Jesús Vázquez",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-14T11:14:54.20329",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 6,
+                "nombre": "Blanca",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-15T17:28:40.426775",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 7,
+                "nombre": "AQUILEO",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-15T18:38:53.168854",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 8,
+                "nombre": "Ricardo Cabrera Lezama ",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-15T19:47:58.968218",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 9,
+                "nombre": "Catalina Lezama Avila",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-15T19:50:52.55565",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 2,
+                "nombre": "Brenda ",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-13T23:11:01.371722",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 10,
+                "nombre": "LAZARO CARLOS CABRERA LEZAMA ",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-15T21:30:05.291558",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 11,
+                "nombre": "Confirmo mi asistencia ",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-15T21:50:27.960662",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 12,
+                "nombre": "Margarita lezama",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-15T21:53:05.963069",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 13,
+                "nombre": "Andrea lezama ",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-16T11:24:24.205151",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 15,
+                "nombre": "Familia Chan Galan ",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-16T18:08:34.152328",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            },
+            {
+                "id": 16,
+                "nombre": "Ana María Salgado ",
+                "acompanantes": null,
+                "fechaConfirmacion": "2025-01-17T21:06:32.153293",
+                "invitacion": {
+                    "id": 1,
+                    "nombre": "Invitacion Itzae",
+                    "fechaCreacion": "2025-01-13T23:08:48.381583"
+                },
+                "valido": true
+            }
+        ],
+        "numeroVisitas": 89,
+        "numeroConfirmados": 13
+    }
+}
